a020ddf8dc4a660955aa320c6a182ad7e2775330
Martin Pecka <peci1@seznam.cz>
2024-11-19 14:26:20 +0100

    Added environment variable GAZEBO_SUPPRESS_EOL_WARNING that disables EOL notices.

--- gazebo/gazebo_shared.cc
+++ gazebo/gazebo_shared.cc
@@ -14,6 +14,8 @@
  * limitations under the License.
  *
  */
+#include <cstdlib>
+
 #include <sdf/sdf.hh>
 
 #include "gazebo/transport/TransportIface.hh"
@@ -27,6 +29,11 @@
 void gazebo_shared::printVersion()
 {
   fprintf(stderr, "%s", GAZEBO_VERSION_HEADER);
+
+  char *ignoreEol = getenv("GAZEBO_SUPPRESS_EOL_WARNING");
+  if (ignoreEol != nullptr && strncmp(ignoreEol, "1", 1) == 0)
+    return;
+
   const char* msg = R"(
 #     # ####### ####### ###  #####  #######
 ##    # #     #    #     #  #     # #
